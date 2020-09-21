9dfec3c49e9561d8c25b99197dad0a85371ed0d2
DESKTOP-Q7U1K2V\serrl <coffeescript@kakao.com>
2020-09-21 20:30:03 +0900

    git init commit

---
+++ src/test/java/com/hello/core/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
